a2fe6344766e6c8fa15e4062cc50de88ed895ca4
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2022-03-11 18:58:29 -0300

    Update startup.sh

--- debian/home/.config/i3/scripts/startup.sh
+++ debian/home/.config/i3/scripts/startup.sh
@@ -10,7 +10,7 @@ i3-msg 'workspace 10; append_layout ~/.config/i3/layouts/workspace_10.json'
 
 # Web Browser
 
-i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox https://gleam.io/4AI4e/sorteio-setup-gamer-chipart-e-cougar https://www.pciconcursos.com.br/concursos/nordeste/ https://www.pciconcursos.com.br/concursos/nacional/ http://mythicspoiler.com/newspoilers.html https://www.robocore.net/kit-raspberry-pi https://www.filipeflop.com/produto/kit-raspberry-pi-mega/ https://www.ligamagic.com.br/?view=colecao/colecao https://www.reddit.com/r/unixporn/new/'
+i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox https://drive.google.com/file/d/1RLei-iWw-zNQDxTZ_UfM5pI52iT3s5jk/view https://gleam.io/4AI4e/sorteio-setup-gamer-chipart-e-cougar https://www.pciconcursos.com.br/concursos/nordeste/ https://www.pciconcursos.com.br/concursos/nacional/ http://mythicspoiler.com/newspoilers.html https://www.robocore.net/kit-raspberry-pi https://www.filipeflop.com/produto/kit-raspberry-pi-mega/ https://www.ligamagic.com.br/?view=colecao/colecao https://www.reddit.com/r/unixporn/new/'
 
 i3-msg 'exec --no-startup-id brave-browser https://www.youtube.com/feed/subscriptions https://www.hltv.org/matches https://onefootball.com/pt-br/time/flamengo-1802'
 
